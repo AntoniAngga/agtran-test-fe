15d55e54f91f9a4c13a29f84331f164be44ccb41
Antoni Angga <antoniangga14@gmail.com>
2020-09-08 11:19:57 +0700

    first init and create Login vue

---
+++ src/router/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Login from '../views/Login.vue'
+import About from '../views/About.vue'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  }
+]
+
+const router = new VueRouter({
+  mode: "history",
+  routes
+})
+
+export default router
